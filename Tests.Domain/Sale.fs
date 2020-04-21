e97fd25662e2e229b699fd7e0bb594b8625623b2
Denis <letto-l@yandex.ru>
2020-04-21 21:22:43 +0300

    Tests Sale - Sum cost of products equals sum of sale

---
+++ Tests.Domain/Sale.fs
@@ -0,0 +1,26 @@
+﻿module Tests.Domain.Sale
+
+open System
+open Domain.Sale
+open FsCheck
+open FsCheck.Xunit
+
+let sum x y =
+  x + y
+  //match x with
+  //| x when x <= 3 -> 10
+  //| _ -> x + y
+
+[<Property>]
+let ``Reverse of reverse of a list is the original list ``(xs:list<int>) =
+  List.rev(List.rev xs) = xs
+  
+[<Property>]
+let ``Sum is shit``(x:int, y:int) =
+  sum x y = x + y
+  
+[<Property>]
+let ``Sum cost of products equals sum of sale`` (products: list<Product>) =
+  products
+  |> List.map(fun x -> x.Cost * (decimal x.Amount))
+  |> List.sum = (createSale products DateTime.Now).Sum
