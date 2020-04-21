13806c2939059fcf060d7c9da5066740ca6e65da
Denis <letto-l@yandex.ru>
2020-04-21 20:39:54 +0300

    Sale createSale

---
+++ Domain/Sale.fs
@@ -0,0 +1,26 @@
+﻿module Domain.Sale
+
+open System
+
+type Product = {
+    ProductId: int
+    Amount: int
+    Cost: decimal
+}
+
+type CreatedSale = {
+    Products: Product list
+    Date: DateTime
+    Sum: decimal
+}
+
+let createSale products date =
+    
+    let createdSale = {
+        Products = products
+        Date = date
+        Sum = products
+               |> List.map(fun x -> (decimal x.Amount) * x.Cost)
+               |> List.sum
+    }
+    createdSale
