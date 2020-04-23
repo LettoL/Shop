bd2ff58890a6670f456c8f34f1c86d329105ad75
Denis <letto-l@yandex.ru>
2020-04-23 12:04:04 +0300

    Application sale commands

---
+++ Application/Commands/Sale.fs
@@ -0,0 +1,24 @@
+﻿module Application.Commands.Sale
+
+open System
+open Domain.Sale
+
+type SaleCreate = {
+  Date: DateTime
+  Products: SoldProduct list
+  Moneys: DepositedMoney list
+}
+
+type SaleCreated = {
+  Id: int
+}
+
+type SaleCommands =
+  | SaleCreate of SaleCreate
+  
+type SaleEvents =
+  | SaleCreated of SaleCreated
+  
+let saleCreateHandler (command: SaleCreate): SaleCreated =
+  let result = { Id = 1 }
+  result
