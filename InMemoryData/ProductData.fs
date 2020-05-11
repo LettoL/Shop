2d5c759e839d50838d8dc66e5bdfa75acfa535f9
Denis <letto-l@yandex.ru>
2020-05-11 15:42:34 +0300

    InMemoryData & SaleData & ProductData

---
+++ InMemoryData/ProductData.fs
@@ -0,0 +1,27 @@
+﻿module InMemoryData.ProductData
+
+open System
+open System.Collections
+
+type ShippedProduct = {
+  Id: int
+  Date: DateTime
+  ProductId: int
+  Amount: int
+}
+
+type Query = All
+  
+type Command = Create of ShippedProduct * Hashtable
+
+let data = Hashtable()
+
+let commandsHandler command =
+  match command with
+  | Create (event, data) ->
+    data.Add(event.Id, event)
+    event.Id
+    
+let queriesHandler query =
+  match query with
+  | All -> data.Values |> Seq.cast |> Array.ofSeq
---
+++ InMemoryData/SaleData.fs
@@ -0,0 +1,25 @@
+﻿module InMemoryData.SaleData
+
+open System.Collections
+
+type Sale = {
+  Id: int
+}
+
+type Query =
+  | All
+  
+type Command =
+  | Create of Sale * Hashtable
+
+let data = Hashtable()
+
+let commandsHandler command =
+  match command with
+  | Create (sale, data) ->
+    data.Add(sale.Id, sale)
+    sale.Id
+  
+let queriesHandler query =
+  match query with
+  | All -> data.Values |> Seq.cast |> Array.ofSeq
