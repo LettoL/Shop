c343f02548eee0f21bd93da58475b371497dfc2b
Denis <letto-l@yandex.ru>
2020-05-12 12:32:34 +0300

    Domain createSale

---
+++ Domain/Product.fs
@@ -0,0 +1,9 @@
+﻿module Domain.Product
+
+open System
+
+type ShippedProduct = {
+  Date: DateTime
+  ProductId: int
+  Amount: int
+}
--- Domain/Sale.fs
+++ Domain/Sale.fs
@@ -1,6 +1,7 @@
 ﻿module Domain.Sale
 
 open System
+open Domain.Product
 
 type SoldProduct = {
     ProductId: int
@@ -14,31 +15,64 @@ type DepositedMoney = {
     PaymentType: PaymentType
 }
 
+type SaleCreate = {
+  Id: Guid
+  Date: DateTime
+  Products: SoldProduct list
+  Moneys: DepositedMoney list
+}
+
+type Sale = {
+  Id: Guid
+  Date: DateTime
+  Products: SoldProduct list
+  Moneys: DepositedMoney list
+  Sum: decimal
+}
+
 type CreatedSale = {
-    Products: SoldProduct list
-    Date: DateTime
-    CostProducts: decimal
+    Sale: Sale
+    ShippedProducts: ShippedProduct list
     DepositedMoneys: DepositedMoney list
 }
 
 type SaleCreateError =
     | DepositedSumNotEqualCostProducts
 
-let calculationCostProducts = List.sumBy(fun x -> (decimal x.Amount) * x.Cost)
-let calculationDepositedSum = List.sumBy(fun x -> x.Sum)
-
-let createSale date products moneys =
- 
-    let costProducts = products |> calculationCostProducts
-    let depositedSum = moneys |> calculationDepositedSum
+let calculationCostProducts =
+    List.sumBy(fun x -> (decimal x.Amount) * x.Cost)
     
-    if costProducts <> depositedSum then
+let calculationDepositedSum (money: DepositedMoney list): decimal = 
+    let result = money |> List.sumBy(fun x -> x.Sum)
+    result
+
+let createSale (command: SaleCreate): Result<CreatedSale, SaleCreateError> =
+ 
+    let sum = command.Products |> calculationCostProducts
+    let depositedSum = command.Moneys |> calculationDepositedSum
+    
+    if sum <> depositedSum then
       Error DepositedSumNotEqualCostProducts
     else
-        let createdSale = {
-            Products = products
-            Date = date
-            CostProducts = costProducts
-            DepositedMoneys = moneys
-        }
-        Ok createdSale
+      let sale = {
+        Id = command.Id
+        Products = command.Products
+        Date = command.Date
+        Sum = sum
+        Moneys = command.Moneys
+      }
+      
+      let shippedProducts = command.Products |> List.map(fun x -> {
+        Date = command.Date
+        ProductId = x.ProductId
+        Amount = x.Amount
+      })
+      
+      let depositedMoney = command.Moneys
+      
+      let result = {
+        Sale = sale
+        ShippedProducts = shippedProducts
+        DepositedMoneys = depositedMoney
+      }
+      Ok result
