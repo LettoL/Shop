e619e388e7bcb99b2856de3284f1226a6669c21d
Denis <letto-l@yandex.ru>
2020-06-15 20:45:50 +0300

    WebAPI Giraffe

---
+++ WebAPI/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Hosting
+open Startup
+
+[<EntryPoint>]
+let main _ =
+    Host.CreateDefaultBuilder()
+      .ConfigureWebHostDefaults(fun webHostBuiler ->
+        webHostBuiler
+          .Configure(configureApp)
+          .ConfigureServices(configureServices)
+          |> ignore)
+      .Build()
+      .Run()
+    0 
---
+++ WebAPI/Startup.fs
@@ -0,0 +1,17 @@
+﻿module Startup
+
+open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+
+let webApp =
+  choose [
+    route "/ping" >=> text "pong"
+    route "/" >=> text "/"
+  ]
+  
+let configureApp (app: IApplicationBuilder) =
+  app.UseGiraffe webApp
+  
+let configureServices (services: IServiceCollection) =
+  services.AddGiraffe() |> ignore
