798b71f38e60f3c9dcc056c8676740d2fdbecd2c
Denis <letto-l@yandex.ru>
2020-06-16 15:24:43 +0300

    clear

--- Application/Commands/Sale.fs
+++ Application/Commands/Sale.fs
@@ -2,7 +2,6 @@
 
 open System
 open Domain.Sale
-open InMemoryData
 
 type SaleCreated = {
   Id: Guid
@@ -14,26 +13,3 @@ type SaleCommands =
 type SaleEvents =
   | SaleCreated of SaleCreated
   
-let saleCreateHandler (command: SaleCreate): Result<SaleCreated, SaleCreateError> =
-  //создать продажу
-  let createdSale = createSale command
-  match createdSale with
-  | Error error -> Error error
-  | Ok createdSale ->
-    let id = SaleData.commandsHandler(SaleData.Create createdSale.Sale)
-    
-    //зачислить деньги
-    //отгрузить товар
-    createdSale.ShippedProducts
-      |> List.map(fun x -> ProductData.commandsHandler (ProductData.Create x))
-      |> ignore
-    
-    Ok { Id = id }
-  
-  //let createdSale = createSale command.Date command.Products command.Moneys
-  //match createdSale with
-  //| Error -> Error DepositedSumNotEqualCostProducts
-  //| Ok createdSale ->
-  //  Console.WriteLine("asd")
-  // let result = { Id = 1 }
-  //  Ok result
